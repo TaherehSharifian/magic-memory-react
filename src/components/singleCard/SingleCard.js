ed2122b2750c34d139e1af61aba4116567efa58a
Tahereh Sharifian <tahereh.sharifian97@gmail.com>
2023-02-08 21:44:48 +0330

    Update SingleCard.js

--- src/components/singleCard/SingleCard.js
+++ src/components/singleCard/SingleCard.js
@@ -13,7 +13,7 @@ const SingleCard = ({ card, handleCardChoices, flipped, disabled }) => {
         <img className="front" src={card.src} alt="card-front" />
         <img
           className="back"
-          src="/img/cover.png"
+          src="img/cover.png"
           alt="card-back"
           onClick={handleClick}
         />
